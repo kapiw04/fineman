77fafd28dfd1d053f03c978b56eabc00c72a7ab4
dominik271828 <dominik.godek@gmail.com>
2024-11-23 20:26:38 +0100

    passing tests

--- API/users/models.py
+++ API/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Account(models.Model):
+    # account_set added implicitly
     name = models.CharField(max_length=64)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
