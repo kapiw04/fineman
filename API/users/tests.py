fd993af4cc6f50fc74807f7f805c4a6ffdcd8633
dominik271828 <dominik.godek@gmail.com>
2024-11-26 13:07:23 +0100

    added some testcases, along with a test view, fixed the typo in configuration, added example private and public keys, installed stubs for mypi

--- API/users/tests.py
+++ API/users/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework import status
 from django.contrib.auth.models import User
+# TODO: remove below lines
+# check if the algo used is in fact rsa512
+
 # Create your tests here.
 client = Client()
 
@@ -10,6 +13,11 @@ class JwtTestCase(TestCase):
         self.user = User.objects.create_user("filip",password="kochamsiostre123")
         self.user.save()
 
+    def test_rsa(self):
+        """Check is RSA is enabled"""
+        from django.conf import settings
+        self.assertEqual(settings.SIMPLE_JWT["ALGORITHM"], "RS512")  
+
     def test_generate_jwt(self):
         body = {"username": "filip", "password": "kochamsiostre123"}
         response = client.post(reverse('token_obtain_pair'), data=body,  format='json')
@@ -17,4 +25,19 @@ class JwtTestCase(TestCase):
         self.assertNotEqual(response.data['access'], "")
         self.assertNotEqual(response.data['refresh'], "")
 
+    def test_generate_jwt_negative(self):
+        body = {"username": "bezimienny", "password": "kochamsiostre123"}
+        response = client.post(reverse('token_obtain_pair'), data=body,  format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_claims_authentication(self):
+        body = {"username": "filip", "password": "kochamsiostre123"}
+        response = client.post(reverse('token_obtain_pair'), data=body,  format='json')
+        headers = {"Authorization": "Bearer " + response.data['access']}
+        verified_view = client.get(reverse('test_jwt_auth'), headers=headers)
+        self.assertEqual(verified_view.status_code, status.HTTP_200_OK)
+
+    def test_claims_authentication_negative(self):
+        headers = {"Authorization": "Bearer " + "invalid token"}
+        verified_view = client.get(reverse('test_jwt_auth'), headers=headers)
+        self.assertEqual(verified_view.status_code, status.HTTP_401_UNAUTHORIZED)
--- API/users/urls.py
+++ API/users/urls.py
@@ -3,8 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 from django.urls import path
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test_jwt_auth', views.TestJwtAuth.as_view(), name='test_jwt_auth')
 ]
