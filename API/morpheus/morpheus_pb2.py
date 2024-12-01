1738223d51a33d475f5dcc6048611f3c561a96a0
Adam Balski <36050429+AdamBalski@users.noreply.github.com>
2024-12-01 23:07:06 +0100

    All morpheus grpc requests interface (#12)
    * add delete product groups request and give feedback request
    
    * fix typo

--- API/morpheus/morpheus_pb2.py
+++ API/morpheus/morpheus_pb2.py
@@ -22,9 +22,10 @@ _runtime_version.ValidateProtobufRuntimeVersion(
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0emorpheus.proto\x12\x08morpheus\"<\n\x1aPredictProductNamesRequest\x12\x0f\n\x07receipt\x18\x01 \x01(\t\x12\r\n\x05token\x18\x02 \x01(\t\".\n\x15PredictedProductNames\x12\x15\n\rpredictedName\x18\x01 \x03(\t\"a\n\x1bPredictProductNamesResponse\x12\x42\n\x19predictedProductNamesList\x18\x01 \x03(\x0b\x32\x1f.morpheus.PredictedProductNames2p\n\x08Morpheus\x12\x64\n\x13PredictProductNames\x12$.morpheus.PredictProductNamesRequest\x1a%.morpheus.PredictProductNamesResponse\"\x00\x42\x0eZ\x0csrc/protogenb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0emorpheus.proto\x12\x08morpheus\x1a\x1bgoogle/protobuf/empty.proto\"3\n\rPredictedName\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x14\n\x0cproductGroup\x18\x02 \x01(\x04\"T\n\x1aPredictProductNamesRequest\x12\r\n\x05token\x18\x01 \x01(\t\x12\x10\n\x08products\x18\x02 \x03(\t\x12\x15\n\rproductGroups\x18\x03 \x03(\x04\"_\n\x15PredictedProductNames\x12/\n\x0epredictedNames\x18\x01 \x03(\x0b\x32\x17.morpheus.PredictedName\x12\x15\n\rnameOnReceipt\x18\x02 \x01(\t\"a\n\x1bPredictProductNamesResponse\x12\x42\n\x19predictedProductNamesList\x18\x01 \x03(\x0b\x32\x1f.morpheus.PredictedProductNames\"3\n\x1a\x44\x65leteProductGroupsRequest\x12\x15\n\rproductGroups\x18\x01 \x03(\x04\"\x97\x01\n\x13SendFeedbackRequest\x12\r\n\x05token\x18\x01 \x01(\t\x12\x38\n\x0fgoodPredictions\x18\x02 \x03(\x0b\x32\x1f.morpheus.PredictedProductNames\x12\x37\n\x0e\x62\x61\x64Predictions\x18\x03 \x03(\x0b\x32\x1f.morpheus.PredictedProductNames2\x90\x02\n\x08Morpheus\x12\x64\n\x13PredictProductNames\x12$.morpheus.PredictProductNamesRequest\x1a%.morpheus.PredictProductNamesResponse\"\x00\x12G\n\x0cSendFeedback\x12\x1d.morpheus.SendFeedbackRequest\x1a\x16.google.protobuf.Empty\"\x00\x12U\n\x13\x44\x65leteProductGroups\x12$.morpheus.DeleteProductGroupsRequest\x1a\x16.google.protobuf.Empty\"\x00\x42\x0eZ\x0csrc/protogenb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,12 +33,18 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'morpheus_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'Z\014src/protogen'
-  _globals['_PREDICTPRODUCTNAMESREQUEST']._serialized_start=28
-  _globals['_PREDICTPRODUCTNAMESREQUEST']._serialized_end=88
-  _globals['_PREDICTEDPRODUCTNAMES']._serialized_start=90
-  _globals['_PREDICTEDPRODUCTNAMES']._serialized_end=136
-  _globals['_PREDICTPRODUCTNAMESRESPONSE']._serialized_start=138
-  _globals['_PREDICTPRODUCTNAMESRESPONSE']._serialized_end=235
-  _globals['_MORPHEUS']._serialized_start=237
-  _globals['_MORPHEUS']._serialized_end=349
+  _globals['_PREDICTEDNAME']._serialized_start=57
+  _globals['_PREDICTEDNAME']._serialized_end=108
+  _globals['_PREDICTPRODUCTNAMESREQUEST']._serialized_start=110
+  _globals['_PREDICTPRODUCTNAMESREQUEST']._serialized_end=194
+  _globals['_PREDICTEDPRODUCTNAMES']._serialized_start=196
+  _globals['_PREDICTEDPRODUCTNAMES']._serialized_end=291
+  _globals['_PREDICTPRODUCTNAMESRESPONSE']._serialized_start=293
+  _globals['_PREDICTPRODUCTNAMESRESPONSE']._serialized_end=390
+  _globals['_DELETEPRODUCTGROUPSREQUEST']._serialized_start=392
+  _globals['_DELETEPRODUCTGROUPSREQUEST']._serialized_end=443
+  _globals['_SENDFEEDBACKREQUEST']._serialized_start=446
+  _globals['_SENDFEEDBACKREQUEST']._serialized_end=597
+  _globals['_MORPHEUS']._serialized_start=600
+  _globals['_MORPHEUS']._serialized_end=872
 # @@protoc_insertion_point(module_scope)
